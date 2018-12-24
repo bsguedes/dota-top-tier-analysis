79783b43abd7faa2c9dda46bfa5c506cf16bef1c
Bruno Guedes <bsguedes@inf.ufrgs.br>
2018-12-24 01:16:39 -0200

    create directories if they dont exist

--- downloader.py
+++ downloader.py
@@ -1,10 +1,12 @@
 import requests
+import os
 import os.path
-
 
 class Downloader:
     @staticmethod
     def download_player_data(players, override=True):
+        if not os.path.exists('players'):
+            os.makedirs('players')
         for name, pid in players.items():
             file_name = 'players/%s_matches.json' % name
             if override or not os.path.isfile(file_name):
@@ -17,6 +19,8 @@ class Downloader:
     def download_matches(unique_matches, override=False):
         print('')
         print('Found %s matches' % len(unique_matches))
+        if not os.path.exists('players'):
+            os.makedirs('players')
         for match_id in unique_matches:
             file_name = 'matches/%s.json' % match_id
             if override or not os.path.isfile(file_name):
